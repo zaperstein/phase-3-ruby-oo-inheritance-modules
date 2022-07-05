e861e2f0c5ee9eb67c786d8512f2585acdc0e2ed
Maggie Saperstein <maggie.saperstein@gmail.com>
2022-07-05 15:14:22 -0500

    commit tree c0b8205ede89e16ea1071f88c5ed9c66cd5150f5

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,2 +1,8 @@
 require_relative './dance_module'
 require_relative './meta_dancing_module'
+require_relative './fancy_dance'
+
+class Dancer
+  extend FancyDance::ClassMethods
+  include FancyDance::InstanceMethods
+end
